46935df140c2757efd76346fb7b3624df41be102
Test User_1 <guzanovand@gmail.com>
2021-12-19 15:22:49 +0700

    Edit Connection class, a class constructor has been added, methods for sending and receiving messages using serialization

--- src/main/java/Connection_AND_ConsoleHelper/Connection.java
+++ src/main/java/Connection_AND_ConsoleHelper/Connection.java
@@ -1,6 +1,45 @@
 package Connection_AND_ConsoleHelper;
 
 
+import Message.Message;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Objects;
 
 public class Connection {
+    private final Socket socket;
+    private final ObjectOutputStream oos;
+    private final ObjectInputStream ois;
+
+
+    public Connection(Socket socket) throws IOException {
+        Objects.requireNonNull(socket);
+        this.socket = socket;
+        this.oos = new ObjectOutputStream(socket.getOutputStream());
+        this.ois = new ObjectInputStream(socket.getInputStream());
+    }
+
+
+    //отправить
+    public void sendMessage(Message message) {
+        synchronized (oos) {
+            try {
+                oos.writeObject(message);
+            } catch (IOException e) {
+                System.out.println("E--->Connection/sendMessage(...):");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //получить
+    public Message receive() throws IOException, ClassNotFoundException {
+        synchronized (ois) {
+            return (Message) ois.readObject();
+        }
+    }
 }
+
